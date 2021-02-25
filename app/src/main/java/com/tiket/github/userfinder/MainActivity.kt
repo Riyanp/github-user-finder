972182ca6480259cbbbb3b16e4b8c0342074aa7d
riyanp <pramana.riyan93@gmail.com>
2021-02-25 15:10:41 +0700

    create view for main activity
    - handle on edit text touch condition

--- app/src/main/java/com/tiket/github/userfinder/MainActivity.kt
+++ app/src/main/java/com/tiket/github/userfinder/MainActivity.kt
@@ -1,15 +1,50 @@
 package com.tiket.github.userfinder
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity
 import com.tiket.github.userfinder.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding : ActivityMainBinding
+    lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        handleSearchAction()
+    }
+
+    private fun handleSearchAction() {
+        with(binding) {
+            etSearch.setOnFocusChangeListener { _, hasFocus ->
+                if (!hasFocus) {
+                    ivLogo.visibility = View.VISIBLE
+                    ivRemove.visibility = View.GONE
+                } else {
+                    ivLogo.visibility = View.GONE
+                    ivRemove.visibility = View.VISIBLE
+                }
+            }
+
+            etSearch.setOnEditorActionListener { _, action, _ ->
+                if (action == EditorInfo.IME_ACTION_SEARCH) {
+                    etSearch.clearFocus()
+                    hideKeyboard()
+                }
+                false
+            }
+        }
+    }
+
+    private fun hideKeyboard() {
+        val imm: InputMethodManager =
+            getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(binding.etSearch.windowToken, 0)
     }
 }
