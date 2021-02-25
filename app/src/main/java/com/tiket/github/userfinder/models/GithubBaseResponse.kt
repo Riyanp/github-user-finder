05744f4dbb8c0097daefcaa642c8ca9b0724cd80
riyanp <pramana.riyan93@gmail.com>
2021-02-25 16:02:52 +0700

    create base response model

---
+++ app/src/main/java/com/tiket/github/userfinder/models/GithubBaseResponse.kt
@@ -0,0 +1,17 @@
+package com.tiket.github.userfinder.models
+
+import android.content.ClipData.Item
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+
+class GithubBaseResponse {
+    @SerializedName("total_count")
+    var totalCount: Int? = null
+
+    @SerializedName("incomplete_results")
+    var incompleteResults: Boolean? = null
+
+    @SerializedName("items")
+    var items: List<UserResponse>? = null
+}
