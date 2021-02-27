b5f7d9f83e8063b13a9f5c481a95894ce909a7b3
riyanp <pramana.riyan93@gmail.com>
2021-02-27 19:59:03 +0700

    create new function to load more data when the recycler is scrolled to the last visible item

--- app/src/main/java/com/tiket/github/userfinder/MainActivity.kt
+++ app/src/main/java/com/tiket/github/userfinder/MainActivity.kt
@@ -9,9 +9,11 @@ import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.tiket.github.userfinder.adapter.UserRecyclerAdapter
 import com.tiket.github.userfinder.databinding.ActivityMainBinding
 import com.tiket.github.userfinder.models.GithubBaseResponse
+import com.tiket.github.userfinder.models.UserResponse
 import com.tiket.github.userfinder.network.Config
 import com.tiket.github.userfinder.network.DataManager
 import com.tiket.github.userfinder.network.NetworkCallback
@@ -19,8 +21,15 @@ import com.tiket.github.userfinder.network.NetworkCallback
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var scrollListener: RecyclerView.OnScrollListener
     private lateinit var binding: ActivityMainBinding
     private lateinit var linearLayoutManager: LinearLayoutManager
+    private lateinit var userAdapter: UserRecyclerAdapter
+
+    private var userList = emptyList<UserResponse>().toMutableList()
+    private var currentPage = 1
+    private var totalItemCount = 0
+    private var queryValue = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,15 +52,16 @@ class MainActivity : AppCompatActivity() {
             }
 
             etSearch.setOnEditorActionListener { _, action, _ ->
-                val seachValue = etSearch.text.toString()
+                queryValue = etSearch.text.toString()
                 if (action == EditorInfo.IME_ACTION_SEARCH) {
-                    if (seachValue.isNotEmpty()) {
-                        fetchApiUserSearch(seachValue)
-                        etSearch.clearFocus()
-                        hideKeyboard()
+                    if (queryValue.isNotEmpty()) {
+                        currentPage = 1
+                        fetchApiUserSearch(currentPage, true)
                     } else {
                         toast(getString(R.string.field_must_not_empty))
                     }
+                    etSearch.clearFocus()
+                    hideKeyboard()
                 }
                 false
             }
@@ -62,38 +72,72 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun fetchApiUserSearch(queryValue: String) {
+    private fun fetchApiUserSearch(page: Int, isFromZero: Boolean) {
+        if (isFromZero) {
+            binding.progressCircular.visibility = View.VISIBLE
+            binding.rvUser.visibility = View.GONE
+        } else {
+            binding.llLoadMore.visibility = View.VISIBLE
+        }
+
         val queryFilter = emptyMap<String, String>().toMutableMap()
         queryFilter["q"] = queryValue
+        queryFilter["page"] = page.toString()
+        queryFilter["per_page"] = "10"
+
         DataManager.getInstance(this).network(
             Config.getAPI().fetchApiSearchUser(queryFilter),
             object : NetworkCallback {
                 override fun onSuccess(model: GithubBaseResponse) {
-                    toast(model.totalCount.toString())
                     if (!model.items.isNullOrEmpty()) {
-                        val data = model.items!!.subList(
-                            0,
-                            if (model.items!!.size > 5) 9 else model.items!!.size
-                        )
-                        val userAdapter =
-                            UserRecyclerAdapter(this@MainActivity, data)
-                        binding.rvUser.apply {
-                            layoutManager = linearLayoutManager
-                            userAdapter.onItemClick = { _, response ->
-                                val browserIntent =
-                                    Intent(Intent.ACTION_VIEW, Uri.parse(response.htmlUrl))
-                                startActivity(browserIntent)
-                            }
-                            adapter = userAdapter
+                        if (isFromZero) {
+                            totalItemCount = model.totalCount ?: 0
+                            userList = model.items!!.subList(0, model.items!!.size)
+                            initUserAdapter()
+                        } else {
+                            binding.llLoadMore.visibility = View.GONE
+                            userList.addAll(model.items!!.subList(0, model.items!!.size))
+                            userAdapter.notifyDataSetChanged()
                         }
+                    } else {
+                        userList.clear()
+                        if (binding.rvUser.adapter != null) {
+                            userAdapter.notifyItemRangeRemoved(0, userList.size)
+                            userAdapter.notifyDataSetChanged()
+                        }
+                        toast(getString(R.string.username_not_found))
                     }
+
+                    binding.rvUser.visibility = View.VISIBLE
+                    binding.progressCircular.visibility = View.GONE
                 }
 
                 override fun onError(message: String?) {
+                    binding.progressCircular.visibility = View.GONE
+                    binding.llLoadMore.visibility = View.GONE
                     toast(message.toString())
                 }
 
             })
+    }
+
+    private fun initUserAdapter() {
+        userAdapter = UserRecyclerAdapter(this@MainActivity, userList)
+        binding.rvUser.apply {
+            layoutManager = linearLayoutManager
+            adapter = userAdapter
+        }
+        binding.rvUser.addOnScrollListener(recyclerScroll)
+        userAdapter.onItemClick = { _, response ->
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(response.htmlUrl)))
+        }
+    }
+
+    private fun isHavingMoreData(): Boolean {
+        if (totalItemCount > userList.size) {
+            return true
+        }
+        return false
     }
 
     private fun hideKeyboard() {
@@ -101,4 +145,26 @@ class MainActivity : AppCompatActivity() {
             getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(binding.etSearch.windowToken, 0)
     }
+
+    private val recyclerScroll: RecyclerView.OnScrollListener =
+        object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                if (dy < 0) {
+                    return
+                }
+                if (linearLayoutManager.findLastCompletelyVisibleItemPosition() == -1
+                    + userAdapter.itemCount
+                ) {
+                    val totalItemCount: Int = linearLayoutManager.itemCount
+                    val lastVisibleItem: Int = linearLayoutManager.findLastVisibleItemPosition()
+                    if (lastVisibleItem > totalItemCount - 2 && dy > 0) {
+                        if (isHavingMoreData()) {
+                            currentPage++
+                            fetchApiUserSearch(currentPage, false)
+                        }
+                    }
+                }
+            }
+        }
 }
